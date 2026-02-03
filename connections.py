d0959e49c9bc5a7cab06ab71f359d3f868265579
Adam Secrest <adam.secrest@csu.fullerton.edu>
2026-02-03 12:05:12 -0800

    Using the socket library, server needs to be central point of messages and storing logs / accounts

--- connections.py
+++ connections.py
@@ -1,7 +1,7 @@
 #This file abstracts the secure connection to a central server
 #Cryptography is handled in this file
 from accounts import authenticate_account, find_account
-
+from cryptograpy_functions import *
 
 
 #Call this function to connect to a channel:
@@ -24,7 +24,14 @@ def connect(user_id, password, channel=None, receiver_id = None):
         else:
             sortedID = [user_id, receiver_id].sorted()
             join_DM(sortedID[0], sortedID[1]) #ensures recall of same chat even if receiver initializes
+    
+    if channel:
+        join_channel(user_id, channel)
 
 def join_DM(id_1, id_2):
+    #TODO
+    pass
+
+def join_channel(user_id, channel_name):
     #TODO
     pass
