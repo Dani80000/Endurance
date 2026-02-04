47e3b02de3c62fb8e4f9db74927e37a2cdcf579d
Dani <ddimovski@csu.fullerton.edu>
2026-02-03 22:01:15 -0800

    (Iteration 2) Implemented server authentication, history loading, and live message logging. Created changes to connections.py as well, regarding joining channels and dms, saving messages, and a typo

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 # This script handles server related operations
 import asyncio,connections, websockets, json
 
-CONNECTED_CLIENTS = set() # Keeps track of connected clients so messages are broadcasted to them
+current_clients = set() # Keeps track of connected clients so messages are broadcasted to them TODO: Add safe removal when a client disconnects during send
 
 async def handler(websocket): # Async will allow us to wait for messages without blocking
     print("New client connected. Waiting for login...")
     
-    try: # This is here in order to void errors from getting bad data to prevent any failures
+    try: # This is here in order to void errors from getting bad data to prevent any failures TODO:(although this should be around things that can fail only)
         message = await websocket.recv() 
         data = json.loads(message)
         
@@ -19,7 +19,7 @@ async def handler(websocket): # Async will allow us to wait for messages without
 
         if connections.connect(user_id, password, channel=target_channel, receiver_id=target_dm):
             
-            CONNECTED_CLIENTS.add(websocket)
+            current_clients.add(websocket)
             print(f"User {user_id} successfully logged in!")
             
             await websocket.send(json.dumps({"status": "success", "message": "Connected securely."}))
@@ -38,7 +38,7 @@ async def handler(websocket): # Async will allow us to wait for messages without
                     
                     connections.save_message_to_json(payload) 
 
-                    for client in CONNECTED_CLIENTS:
+                    for client in current_clients:
                         if client != websocket:
                             await client.send(json.dumps(payload))
                 
@@ -52,8 +52,8 @@ async def handler(websocket): # Async will allow us to wait for messages without
     except Exception as e:
         print(f"Error handling client: {e}")
     finally:
-        if websocket in CONNECTED_CLIENTS:
-            CONNECTED_CLIENTS.remove(websocket)
+        if websocket in current_clients:
+            current_clients.remove(websocket)
 
 async def main():
     async with websockets.serve(handler, "localhost", 8765):
